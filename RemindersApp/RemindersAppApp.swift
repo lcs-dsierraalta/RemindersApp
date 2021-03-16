a9ff856fa173d0b186911f5bb9870ae20687ad6b
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-03-16 15:10:23 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Diego Sierraalta on 2021-03-16.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
